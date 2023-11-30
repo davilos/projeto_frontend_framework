e577849512fdb251b37a761cbe11f92ba6e435da
davilos <daviloscostagg@hotmail.com>
2023-11-30 19:21:52 -0300

    adds new members

--- js/membersScript.js
+++ js/membersScript.js
@@ -1,17 +1,38 @@
 const membros = [
     {
-        nome: "Dávilos",
+        nome: "Dávilos Tavares da Costa",
         foto: "img/members/davilos.jpg",
-        competencias: "Python, Django, Django REST Framework, Docker, Java",
+        competencias: "Python, Django, Django REST Framework, Docker, Java.",
         experiencia: "Nenhuma",
         escolaridade: "Ensino Superior"
     },
     {
-        nome: "Fulano",
-        foto: "img/members/fulano.jpg",
-        competencias: "HTML, CSS e JavaScript",
+        nome: "Kauan Ferreira dos Santos",
+        foto: "img/members/kaua.jpg",
+        competencias: "Pacote Office, Resolução de problemas e Conhecimentos em software.",
+        experiencia: "Atendente da loja Gostosuras da Li",
+        escolaridade: "Ensino superior"
+    },
+    {
+        nome: "Natalia Freitas da Luz Gusmão",
+        foto: "img/members/natalia.jpg",
+        competencias: "Pacote Office, Excel e Conhecimento em sistemas de rede comerciais.",
+        experiencia: "Vendedora na loja Fini",
+        escolaridade: "Ensino superior"
+    },
+    {
+        nome: "Pedro Lucas de s.c. Barros",
+        foto: "img/members/pedro.jpg",
+        competencias: "C#, Unity.",
         experiencia: "Nenhuma",
-        escolaridade: "Ensino Superior"
+        escolaridade: "Ensino superior"
+    },
+    {
+        nome: "Mattheaus Hoffman Franco",
+        foto: "img/members/mattheaus.jpg",
+        competencias: "Banco de Dados SQL, Lógica de Programação com Python e Análise de Dados com Python.",
+        experiencia: "Nenhuma",
+        escolaridade: "Ensino superior"
     },
 ];
 
@@ -30,11 +51,17 @@ function sectionMembros(membro) {
 }
 
 function exibirMembros() {
-    const container = document.getElementById('members-container');
+    const container1 = document.getElementById('members-container-1');
+    const container2 = document.getElementById('members-container-2');
 
-    membros.forEach(membro => {
+    membros.forEach((membro, index) => {
         const membroHTML = sectionMembros(membro);
-        container.insertAdjacentHTML('beforebegin', membroHTML);
+
+        if (index < 3) {
+            container1.insertAdjacentHTML('beforeend', membroHTML);
+        } else {
+            container2.insertAdjacentHTML('beforeend', membroHTML);
+        }
     });
 }
 
